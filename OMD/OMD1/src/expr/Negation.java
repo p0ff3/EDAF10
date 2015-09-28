6070263111b1da3f253a44d5f2591b2a49ea8d10
Tobbe31 <tobias94olsson@gmail.com>
2015-09-28 22:47:29 +0200

    Stuff y'know

--- OMD/OMD1/src/expr/Negation.java
+++ OMD/OMD1/src/expr/Negation.java
@@ -4,24 +4,25 @@ import java.util.Map;
 import java.util.Set;
 
 public class Negation extends Expr {
-		private Variable v1;
-	public Negation(Variable v1){
-		this.v1 = v1;
+	private Expr e1;
+	private boolean b;
+	public Negation(Expr e1){
+		this.e1 = e1;
 	}
 
 	@Override
 	protected void collectVariables(Set<Variable> set) {
-		set.add(v1);
-
+		set.add(new Variable (toString()));
 	}
 
 	@Override
 	public boolean value(Map<Variable, Boolean> map) {
-		return !(map.get(v1));
+		b = !(map.get(e1));
+		return b;
 	}
 	
 	public String toString(){
-		return "SCHWARZENEGGER" + " " +v1.toString();
+		return "SCHWARZENEGGER" + " " + e1.toString();
 	}
 
 }
