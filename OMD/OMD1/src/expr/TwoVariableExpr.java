3541685c4ba55cd88147e1b7f8010c2989a6a261
Povel Larsson <povel.larsson@gmail.com>
2015-09-28 21:09:51 +0200

    Made twovarExpr abstract and remove value-method

--- OMD/OMD1/src/expr/TwoVariableExpr.java
+++ OMD/OMD1/src/expr/TwoVariableExpr.java
@@ -3,7 +3,7 @@ package expr;
 import java.util.Map;
 import java.util.Set;
 
-public class TwoVariableExpr extends Expr {
+abstract class TwoVariableExpr extends Expr {
 	protected Variable v1, v2;
 	
 	protected TwoVariableExpr(Variable v1, Variable v2){
@@ -17,11 +17,4 @@ public class TwoVariableExpr extends Expr {
 		set.add(v2);
 
 	}
-
-	@Override
-	public boolean value(Map<Variable, Boolean> map) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 }
