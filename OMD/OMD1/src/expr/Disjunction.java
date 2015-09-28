96e24fa028bb583a88214e2bc45a624dd1a578f6
Povel Larsson <povel.larsson@gmail.com>
2015-09-28 16:42:13 +0200

    Well, doing alst commit better

---
+++ OMD/OMD1/src/expr/Disjunction.java
@@ -0,0 +1,20 @@
+package expr;
+
+import java.util.Map;
+import java.util.Set;
+
+public class Disjunction extends Expr {
+
+	@Override
+	protected void collectVariables(Set<Variable> set) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean value(Map<Variable, Boolean> map) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
