c6e0982c05aa094026cae681906ff03fd6532be3
Tobbe31 <tobias94olsson@gmail.com>
2015-09-28 18:30:55 +0200

    Fixat Disjunction

--- OMD/OMD1/src/expr/Disjunction.java
+++ OMD/OMD1/src/expr/Disjunction.java
@@ -4,17 +4,27 @@ import java.util.Map;
 import java.util.Set;
 
 public class Disjunction extends SubExpr {
+	private Variable v1, v2;
+
+	public Disjunction(Variable v1, Variable v2) {
+		this.v1 = v1;
+		this.v2 = v2;
+	}
 
 	@Override
 	protected void collectVariables(Set<Variable> set) {
-		// TODO Auto-generated method stub
+		set.add(v1);
+		set.add(v2);
 
 	}
 
 	@Override
 	public boolean value(Map<Variable, Boolean> map) {
-		// TODO Auto-generated method stub
-		return false;
+		if (map.get(v1) && !map.get(v2) || !map.get(v1) && map.get(v2)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
