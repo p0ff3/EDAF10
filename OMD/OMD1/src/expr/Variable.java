3cf769e53075f0f8a6b9b1611976c3fad5b28cbd
Povel Larsson <povel.larsson@gmail.com>
2015-09-28 20:59:33 +0200

    Fixed extension of Variable to Expr from TwoVarExpr

--- OMD/OMD1/src/expr/Variable.java
+++ OMD/OMD1/src/expr/Variable.java
@@ -3,7 +3,7 @@ package expr;
 import java.util.Map;
 import java.util.Set;
 
-public class Variable extends TwoVariableExpr {
+public class Variable extends Expr {
 	private String name;
 
 	public Variable(String name) {
