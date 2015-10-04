bf64fc230ea6d6d40bb99d5f929f6f31d2beaa15
Povel Larsson <povel.larsson@gmail.com>
2015-10-04 22:19:40 +0200

    Allt möjligt skit

--- OMD/OMD2/src/predicate/SimpleTestPredicate.java
+++ OMD/OMD2/src/predicate/SimpleTestPredicate.java
@@ -6,7 +6,7 @@ import ForAll;
 import term.SimpleTestTerm;
 import term.TermList;
 import term.Variable;
-import expr.Implies;;
+//import expr.Implies;;
 
 public class SimpleTestPredicate extends SimpleTestTerm {
     private Variable z = new Variable("z"); // z
