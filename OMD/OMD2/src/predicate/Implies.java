1e644846846b95df35633b3ae79f17b445732af0
Povel Larsson <povel.larsson@gmail.com>
2015-10-05 10:23:29 +0200

    Felaktig import i Implies

--- OMD/OMD2/src/predicate/Implies.java
+++ OMD/OMD2/src/predicate/Implies.java
@@ -1,17 +1,11 @@
 package predicate;
 
-import java.util.function.Predicate;
-
 import term.Term;
 import term.Variable;
 
 public class Implies implements Expr {
 
 	public Implies(Predicate px, Predicate py) {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Implies(predicate.Predicate px, predicate.Predicate py) {
 		// TODO Auto-generated constructor stub
 	}
 
