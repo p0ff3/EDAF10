c0c0f3675a71a446c32fb32be3c6acd7cb9480d7
Povel Larsson <povel.larsson@gmail.com>
2015-10-05 14:24:54 +0200

    Fixed substitute in Variable and Constant

--- OMD/OMD2/src/term/Constant.java
+++ OMD/OMD2/src/term/Constant.java
@@ -3,23 +3,31 @@ package term;
 import java.util.Set;
 
 public class Constant implements Term {
-    private String value;
+	private String value;
 
-    public Constant(String value) {
-        this.value = value;
-    }
+	public Constant(String value) {
+		this.value = value;
+	}
 
-    public Set<Variable> collectVariables(Set<Variable> set) {
-        return set;
-    }
+	public Set<Variable> collectVariables(Set<Variable> set) {
+		return set;
+	}
 
-    public String toString() {
-        return value;
-    }
+	public String toString() {
+		return value;
+	}
 
+	/*
+	 * printBoth(a, a.substitute(x, y)); // a[x\y] = a printBoth(y,
+	 * x.substitute(x, y)); // x[x\y] = y printBoth(a, y.substitute(y, a)); //
+	 * y[y\a] = a printBoth(y, y.substitute(x, y)); // y[x\y] = y
+	 */
 	@Override
 	public Term substitute(Variable x, Term term) {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.equals(x) && !this.equals(term)) {
+			return new Constant(term.toString());
+		} else {
+			return new Constant(value);
+		}
 	}
 }
--- OMD/OMD2/src/term/Variable.java
+++ OMD/OMD2/src/term/Variable.java
@@ -4,51 +4,61 @@ import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Variable implements Term {
-    private static int counter;
-    private String name;
+	private static int counter;
+	private String name;
 
-    public Variable() {
-        this.name = "v" + counter;
-        counter++;
-    }
+	public Variable() {
+		this.name = "v" + counter;
+		counter++;
+	}
 
-    public Variable(String name) {
-        this.name = name;
-        if (Pattern.matches("v\\d+", name)) {
-            throw new RuntimeException("Variable: the name '" + name
-                    + "' is not allowed");
-        }
-    }
+	public Variable(String name) {
+		this.name = name;
+		if (Pattern.matches("v\\d+", name)) {
+			throw new RuntimeException("Variable: the name '" + name + "' is not allowed");
+		}
+	}
 
-    public static void resetCounter() {
-        counter = 0;
-    }
+	public static void resetCounter() {
+		counter = 0;
+	}
 
-    public Set<Variable> collectVariables(Set<Variable> set) {
-        set.add(this);
-        return set;
-    }
+	public Set<Variable> collectVariables(Set<Variable> set) {
+		set.add(this);
+		return set;
+	}
 
-    public boolean equals(Object object) {
-        if (object instanceof Variable) {
-            Variable other = (Variable) object;
-            return name.equals(other.name);
-        } else {
-            return false;
-        }
-    }
+	public boolean equals(Object object) {
+		if (object instanceof Variable) {
+			Variable other = (Variable) object;
+			return name.equals(other.name);
+		} else {
+			return false;
+		}
+	}
 
-    public int hashCode() {
-        return name.hashCode();
-    }
+	public int hashCode() {
+		return name.hashCode();
+	}
 
-    public String toString() {
-        return name;
-    }
-
+	public String toString() {
+		return name;
+	}
+	/*
+    printBoth(a, a.substitute(x, y)); // a[x\y] = a
+    printBoth(y, x.substitute(x, y)); // x[x\y] = y
+    printBoth(a, y.substitute(y, a)); // y[y\a] = a
+    printBoth(y, y.substitute(x, y)); // y[x\y] = y
+	*/
 	@Override
 	public Term substitute(Variable x, Term term) {
-		// TODO Auto-generated method stub
-		return null;
+
+		if (this.equals(x) && !this.equals(term)) {
+			return new Variable(term.toString());
+		} else {
+			return new Variable(name);
+		}
+		//return null;
+
 	}
 }
