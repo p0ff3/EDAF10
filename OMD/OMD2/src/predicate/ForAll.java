835206163f09051b35274a2348775ad23795fcd1
Tobias94Olsson <dat14tol@student.lu.se>
2015-10-06 13:17:01 +0200

    renamed predicate > expr

--- OMD/OMD2/src/predicate/ForAll.java
+++ OMD/OMD2/src/predicate/ForAll.java
@@ -5,30 +5,30 @@ import term.TermList;
 import term.Variable;
 
 public class ForAll implements Expr {
-	private Expr predicate;
+	private Expr expr;
 	private Variable x;
 	
 	public ForAll(Variable v0, Expr predicate) {
 		this.x = v0;
-		this.predicate = predicate;
+		this.expr = predicate;
 	}
 
 	@Override
 	public ForAll substitute(Variable y, Term term) {
 		if(x.equals(y)){
-			return new ForAll(x, predicate);
+			return new ForAll(x, expr);
 		}
 		else if(!x.equals(y) && !term.contains(x)){
-			return new ForAll(x, predicate.substitute(y, term));
+			return new ForAll(x, expr.substitute(y, term));
 		}
 		else{
 			Variable v1 = new Variable();
-			return new ForAll(v1, predicate.substitute(x, v1).substitute(y, term));
+			return new ForAll(v1, expr.substitute(x, v1).substitute(y, term));
 		}
 	}
 	
 	@Override
 	public String toString(){
-		return "FORALL " + x.toString() + " . " + predicate.toString();
+		return "FORALL " + x.toString() + " . " + expr.toString();
 	}
 }
